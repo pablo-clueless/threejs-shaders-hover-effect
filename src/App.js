365085f196774d75cc8198d3ffa85246e88704ee
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-08 23:00:10 +0100

    shaders hover-effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import hoverEffect from 'hover-effect'
 
-function App() {
+import image from './images/image.jpg'
+import image2 from './images/image2.jpg'
+import overlay from './images/overlay.jpg'
+
+const App = () => {
+  useEffect(() => {
+      var image_animate = new hoverEffect({
+          parent: document.querySelector('.item-image'),
+          intensity: 0.5,
+          image1: image,
+          image2: image2,
+          displacementImage: overlay
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='App'>
+      <div className='container'>
+          <div className='item'>
+              <div className='item-header'>
+                  <div className='item-header-heading'>
+                      #endsars
+                  </div>
+                  <div className='item-header-heading2'>
+                      20/10/20
+                  </div>
+              </div>
+              <div className='item-image'></div>
+              <div className='item-footer'>
+                  <div className='item-footer-qoute'>
+                      fighting for our lives.
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  )
 }
 
-export default App;
+export default App
